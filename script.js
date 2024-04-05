420a65d9f8f4683134cb7508706e8b40fcdb5974
Tina Bruce <tina@fishface.se>
2024-04-05 09:59:59 +0200

    Add an item to the list and change background when clicked on

--- script.js
+++ script.js
@@ -2,22 +2,32 @@ const list = document.querySelector('#list');
 const taskList = list.querySelectorAll('li');
 const form = document.querySelector('#taskForm');
 const task = form['task'];
+//console.log({list.innerHTML});
+taskList.forEach(li => console.log(li.innerHTML));
 
 function addTaskToList(event) {
     event.preventDefault();
     list.innerHTML += `
     <li>${task.value}</li>`
-
+    console.log(taskList);
     form.reset();
 }
 
 function changeBackGround(event) {
-    console.log(event.target);
-    //.classList()
+    //console.log(event.target.value);
+    event.target.classList.toggle('text-decoration-line-through');
+    event.target.classList.toggle('bg-success');
 }
 // add event listeners
-taskList.forEach(li => {
-    li.addEventListener('click', changeBackGround)
-});
+
+taskList.forEach(li => li.addEventListener('click', (e) =>
+//(e) => console.log('som', e.target)));
+//e.target.classList.toggle('bg-success')));
+changeBackGround(e)));
+
+
+document.querySelectorAll('p')
+.forEach(p=>p.addEventListener('click', (e)=>
+    console.log('Working', e.target)));
 
 form.addEventListener('submit', addTaskToList);
