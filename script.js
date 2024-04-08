1619f9f4a31c0f9f574c07165c9b3e95c0e452fd
Tina Bruce <tina@fishface.se>
2024-04-08 16:22:27 +0200

    Change insertion into DOM method to  insertAdjacentHTML

--- script.js
+++ script.js
@@ -2,54 +2,33 @@ const form = document.querySelector('#taskForm');
 const task = form['task'];
 const list = document.querySelector('#list');
 
-// Eventlisteners
-form.addEventListener('submit', addTaskToList);
-
 // Functions
 function addTaskToList(event) {
 
     event.preventDefault();
-    list.innerHTML += 
+    list.insertAdjacentHTML('beforeend', 
     `
-        <li>
+        <li class="list-group-item mb-3">
             ${task.value} 
-        <button class="deleteBtn">X</button>
+            <button class="deleteBtn">X</button>
         </li>
-    `
-    form.reset();
-    // const taskItem = list.querySelector('li:last-of-type');
-    // taskItem.addEventListener('click', changeBackGround);
+    `);
 
-    // const deleteBtn = document.querySelector('.deleteBtn:last-of-type');
-    // console.log(deleteBtn);
-    // deleteBtn.addEventListener('click', deleteTask);
-
-    // get all tasks and listen for click
-    const taskList = list.querySelectorAll('li');
-    taskList.forEach(li => li.addEventListener('click',
-    changeBackGround));
-
-    // get all delete buttons and listen for click
-    const deleteBtns = list.querySelectorAll('.deleteBtn');
-    deleteBtns.forEach(btn => btn.addEventListener('click', 
-    deleteTask));
-}
-
-function changeBackGround(event) {
-    event.target.classList.toggle('completed');
-    //event.stopPropagation();
-}
-
-function deleteTask(event) {
-    console.log('hej!');
-
-    // these two do the same thing - remove the button:
-    //event.target.remove();
-    //console.log(this.remove());
-
+    let lastChild = list.lastElementChild;
+    lastChild.addEventListener('click', changeBackGround);
+    lastChild.querySelector('button').addEventListener('click', deleteTask);
     
-    // removes both task and button:
-    event.target.parentNode.remove();
-    //event.stopPropagation();
+    form.reset();
 }
 
+function changeBackGround() {
+    this.classList.toggle('completed');
+}
+
+function deleteTask() {
+    // removes both task and button:
+    this.parentNode.remove();
+}
+
+// Event listeners
+form.addEventListener('submit', addTaskToList);
