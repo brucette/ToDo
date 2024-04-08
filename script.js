8b51706ebc61e20894aceed9155aabfc2c027f05
Tina Bruce <tina@fishface.se>
2024-04-08 10:09:52 +0200

    Add delete button

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
-const list = document.querySelector('#list');
-const taskList = list.querySelectorAll('li');
 const form = document.querySelector('#taskForm');
 const task = form['task'];
-//console.log({list.innerHTML});
-taskList.forEach(li => console.log(li.innerHTML));
+const deleteBtns = document.querySelectorAll('.deleteBtn');
+const list = document.querySelector('#list');
+//const taskList = list.querySelectorAll('li');
 
+// Eventlisteners
+form.addEventListener('submit', addTaskToList);
+
+//taskList.forEach(li => li.addEventListener('click', (event) =>
+//changeBackGround(event)));
+
+// deleteBtns.forEach(btn => btn.addEventListener('click', (event) =>
+// deleteTask(event)));
+
+
+// Functions
 function addTaskToList(event) {
+
     event.preventDefault();
-    list.innerHTML += `
-    <li>${task.value}</li>`
-    console.log(taskList);
+    list.innerHTML += 
+    `
+        <li>
+            ${task.value} 
+        <button class="deleteBtn">X</button>
+        </li>
+    `
+    // let newItem = document.createElement('li');
+    // let itemText = document.createTextNode(task.value);
+    // newItem.appendChild(itemText);
+    // let button = document.createElement('button');
+    // let buttonText = document.createTextNode('X');
+    // button.appendChild(buttonText);
+    // newItem.appendChild(button);
+    // list.appendChild(newItem);
+
     form.reset();
+    //console.log({deleteBtns});
+    //console.log({taskList});
+    const taskItem = list.querySelector('li:last-of-type');
+    taskItem.addEventListener('click', changeBackGround);
+
+    const deleteBtn = document.querySelector('.deleteBtn:last-of-type');
+    console.log(deleteBtn);
+    deleteBtn.addEventListener('click', deleteTask);
 }
 
 function changeBackGround(event) {
-    //console.log(event.target.value);
-    event.target.classList.toggle('text-decoration-line-through');
-    event.target.classList.toggle('bg-success');
+    event.target.classList.toggle('completed');
+    //event.stopPropagation();
 }
-// add event listeners
 
-taskList.forEach(li => li.addEventListener('click', (e) =>
-//(e) => console.log('som', e.target)));
-//e.target.classList.toggle('bg-success')));
-changeBackGround(e)));
+function deleteTask(event) {
+    //event.target.remove();
+   //console.log({deleteBtns});
+    console.log('hej!');
+    //console.log(this.remove());
+    //console.log(event.target.parentNode.remove());
+    //event.stopPropagation();
+}
 
-
-document.querySelectorAll('p')
-.forEach(p=>p.addEventListener('click', (e)=>
-    console.log('Working', e.target)));
-
-form.addEventListener('submit', addTaskToList);
